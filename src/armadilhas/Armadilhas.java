6d97e57d4ea581d379b8edc8a66ba5e76d3ac437
vifelisberto <vi.felis@hotmail.com>
2018-05-19 21:09:00 -0300

    Armadilhas OK

--- src/armadilhas/Armadilhas.java
+++ src/armadilhas/Armadilhas.java
@@ -1,32 +1,23 @@
-/*
-*Jogo das armadilhas
-3 peças - Jogadores
-48 quadrados - Mais o inicio e fim (50)
-
-Todas começam no inicio
-
-A cada turno os jogadores jogam um par de dados
-Peças andam pra frente com a soma dos pontos dos dados (menos perto da ultima casa)
-
-Se cair na armadilha, perde a vez no próximo turno
-
-3 armadilhas ( 15, 23, 36 )
-
-vencedor - chegar no fim
-
-se a soma dos pontos do dado for mais que o necessario para chegar no fim, 
-a quantidade "a mais" deve ser retrocedido
-
-2 dados
- */
 package armadilhas;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 /**
+ * @authors vfelisberto(21034071), Felipe(RA), Gustavo(RA)
  *
- * @author vfelisberto(21034071), Felipe, Gustavo
+ * JOGO DAS ARMADILHAS 3 Jogadores e 48 posições (mais início e fim 50, o vetor
+ * se inicia em 0 e vai até 49) Todos começam no inicio(posição 0)
+ *
+ * A cada turno os jogadores jogam um par de dados As Peças andam pra frente
+ * somandos os pontos existentes com a soma dos pontos dos dados, Se a soma dos
+ * pontos dos dados for mais que o necessário para se chegar ao fim(49), A
+ * quantidade ultrapassada "a mais" deve ser retrocedida.
+ *
+ * Se cair em alguma armadilha, o jogador perde a vez no próximo turno. São 3
+ * armadilhas nas posições 15, 23, 36
+ *
+ * Vencedor - Quando existe algum jogador na posição 49
  */
 public class Armadilhas {
 
@@ -38,14 +29,7 @@ public class Armadilhas {
         String[] jogadores = new String[3];
         int[] posicaoJogador = new int[3];
         boolean[] jogaTurno = new boolean[3];
-        //Variavel usada para saber se existe ganhador
-        boolean semGanhador = true;
-
-        //String[] tabuleiro = new String[50];
-        //Contadores
         int QtdTurnos = 0;
-
-        //Utilitarios
         Scanner sc = new Scanner(System.in);
 
         //Solicita o nome de cada jogador e armazena no vetor Jogadores
@@ -59,77 +43,76 @@ public class Armadilhas {
         }
 
         //Apresenta-se os jogadores que entraram no jogo
-        System.out.println("Esses são os jogadores: " + Arrays.toString(jogadores));;
-        //ler os valores que estão cada casa
-        /*for (int j = 0; j < 3; j++) {
-            System.out.println("Jogador " + (j + 1) + ": " + jogadores[j]);
-        }*/
+        System.out.println("Esses são os jogadores: " + Arrays.toString(jogadores));
+        System.out.println();
 
-        //Aqui o jogo é "iniciado", onde se inicia os turnos etc
-        //Enquanto não tiver ganhador, continua a execução
-        while (semGanhador) {
-            //Ao iniciar um turno é somado mais 1
-            QtdTurnos++;
+        //Aqui começa as ações do jogo
+        //Enquanto não tiver ganhador, continua a execução para sempre
+        while (true) {
+            QtdTurnos++;//Ao iniciar um turno é somado mais 1
 
-            //Para cada jogador, se ele estiver apto a jogar, é feito as jogadas
+            //Para cada jogador, se ele estiver apto a jogar, é pedido para os dados serem jogados e as informações são exibidas em tela.
             for (int i = 0; i < jogadores.length; i++) {
                 if (jogaTurno[i]) {
                     //Chamar jogador na posição i para jogar os dados.
-                    System.out.println("Sua vez " + jogadores[i] + "! \n Por favor jogue os dados!");
+                    System.out.println("Sua vez " + jogadores[i] + "! \n Por favor digite algo e presione enter para jogar os dados!");
 
-                    int dado1 = JogaDado();
-                    int dado2 = JogaDado();
-                    int pontosDados = dado1 + dado2;
+                    if (sc.next() != null) {
+                        int pontosDados = JogaDados(); //Método que joga e exibe o valor dos dois dados
 
-                    System.out.println("Posição Atual: " + posicaoJogador[i]);
-                    System.out.println("1º Dado: " + dado1 + " | 2º Dado: " + dado2 + " | Totalizando: " + pontosDados);
+                        System.out.println("Posição Anterior: " + posicaoJogador[i]);
 
-                    //Altera o posição do jogador atual
-                    posicaoJogador[i] += pontosDados;
+                        //Altera o posição do jogador atual
+                        posicaoJogador[i] += pontosDados;
 
-                    //49 representa a casa 48 do tabuleiro (vetores se iniciam na posição 0)
-                    if (posicaoJogador[i] > 49) {
-                        int casasRetrocesso = (posicaoJogador[i] - 49);
-                        System.out.println("Poxa, a quantidade de pontos nos dados ultrapassou o fim do tabuleiro :( em " + casasRetrocesso + " casa(s), então você irá voltar " + casasRetrocesso + " casa(s).");
+                        //49 representa a casa Final do tabuleiro
+                        if (posicaoJogador[i] > 49) {
+                            int casasRetrocesso = posicaoJogador[i] - 49;
+                            System.out.println("Poxa, a quantidade de pontos nos dados ultrapassou o fim do tabuleiro :( em " + casasRetrocesso + " casa(s), então você irá voltar " + casasRetrocesso + " casa(s) em relação a última casa.");
 
-                        posicaoJogador[i] = 49 - casasRetrocesso;
-                        System.out.println("Você está na posição: " + posicaoJogador[i] + " do tabuleiro.");
+                            posicaoJogador[i] = 49 - casasRetrocesso;
+                            System.out.println("Você está na posição: " + posicaoJogador[i] + " do tabuleiro.");
+                        } else {
+                            System.out.println("Você está na posição: " + posicaoJogador[i] + " do tabuleiro.");
+                            //Armadilhas, caso o jogador esteja na casa 13 ou 23 ou 36 ele não poderar jogar no proximo turno
+                            jogaTurno[i] = (posicaoJogador[i] != 13 && posicaoJogador[i] != 23 && posicaoJogador[i] != 36);
 
-                        jogaTurno[i] = false;
-                    } else {
-                        System.out.println("Você está na posição: " + posicaoJogador[i] + " do tabuleiro.");
-                        jogaTurno[i] = (posicaoJogador[i] != 13 && posicaoJogador[i] != 23 && posicaoJogador[i] != 36);
-                    }
+                            if (!jogaTurno[i]) {
+                                System.out.println("Você caiu na armadilha :(, não irá poder jogar no próximo turno.");
+                            }
+                        }
 
-                    if (posicaoJogador[i] == 49) {
-                        semGanhador = false;
+                        if (posicaoJogador[i] == 49) {
+                            System.out.println("Quantidade de Turnos: " + QtdTurnos);
+                            System.out.println("Ganhador é: " + jogadores[i]);
+                            return;
+                        }
                     }
                 } else {
-                    System.out.println("Que pena! esse turno você não joga," + jogadores[i] + " você caiu em uma armadilha no turno anterior :/");
-                    jogaTurno[i] = true;
+                    System.out.println("Que pena! esse turno você não joga " + jogadores[i] + ", você caiu em uma armadilha no turno anterior :/");
+                    jogaTurno[i] = true; //No próximo turno o jogador poderá jogar.
                 }
+
+                System.out.println();
             }
 
-            semGanhador = !VerificaGanhador(posicaoJogador);
-
         }
-
-        System.out.println("Ganhador é: " + jogadores[i]);
     }
 
-    public static int JogaDado() {
-        int pontos = (int) (Math.random() * 6) + 1;
+    public static int JogaDados() {
+        int dado1 = JogaDado();
+        int dado2 = JogaDado();
+        int pontos = dado1 + dado2;
+
+        System.out.println("1º Dado: " + dado1 + " | 2º Dado: " + dado2 + " | Totalizando: " + pontos);
 
         return pontos;
     }
 
-    public static boolean VerificaGanhador(int[] posicao) {
-        for (int i = 0; i < posicao.length; i++) {
-            if (posicao[i] == 49) {
-                return true;
-            }
-        }
+    public static int JogaDado() {
+        //Gera números randomicos de 1 a 6
+        int pontos = (int) (Math.random() * 6) + 1;
 
-        return false;
+        return pontos;
     }
 }
